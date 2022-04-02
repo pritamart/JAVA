8749500377de6ae348fcf809715d74e1a02284bd
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2022-04-02 20:42:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
+//1st java program 
+//print hello Wolrld
 import java.io.*;
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Wolrld");
     }
 }
