2ea5deb9115a95fe4b505e53604949dfe11aeee0
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2022-04-02 20:59:36 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Variables {
+    public static void main(String[] args) {
+        //integer
+        int a = 5;
+        //float
+        //int a=f;
+        //Compile time error 
+        float b = a;
+        //charater
+        char var = 'A';
+        System.out.println(a+" "+var+" "+b);
+    }
+}
