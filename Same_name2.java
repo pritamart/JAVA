99f45bf6bdc60c60c613fd0af489ab57d016391c
Pritam Samui <77156502+Pritamart@users.noreply.github.com>
2023-06-20 22:17:35 +0530

    Add files via upload

---
+++ Same_name2.java
@@ -0,0 +1,7 @@
+public class Same_name2 {
+     public static void main(String[] args) {
+    same_name myCar = new same_name();     // Create a myCar object
+    myCar.fullThrottle();      // Call the fullThrottle() method
+    myCar.speed(200);          // Call the speed() method
+  }
+}
